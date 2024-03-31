d83785c3785efd6c94fb0d4b7719e3e36e2f1e2f
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-03-31 13:37:49 +0100

    resolved the responsible desing

--- time-minder/profile/script.js
+++ time-minder/profile/script.js
@@ -1,24 +1,6 @@
 const iconoPanel = document.getElementById("icono-panel");
 const barraLateral = document.querySelector(".barra-lateral");
 const spans = document.querySelectorAll("span");
-const menu = document.querySelector(".menu")
-
-menu.addEventListener("click", ()=>{
-    barraLateral.classList.toggle("max-barra-lateral");
-    if (barraLateral.classList.contains("max-barra-lateral")) {
-        menu.children[0].style.display="none";
-        menu.children[1].style.display="block";
-    } else {
-        menu.children[0].style.display="block";
-        menu.children[1].style.display="none";
-    }
-    if (window.innerWidth<=320) {
-        barraLateral.classList.add("barra-reducida");
-        spans.forEach((span)=> {
-            span.classList.add("oculto");
-        })
-    }
-})
 
 iconoPanel.addEventListener("click", ()=>{
     barraLateral.classList.toggle("barra-reducida");
