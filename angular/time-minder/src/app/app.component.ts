cd043bc1e9e41cd564f5130a27e9e79ca525f3a6
XxMARRExX <xx_marre_xx@hotmail.com>
2024-04-03 20:13:01 +0100

    Creating my first component in Angular

--- angular/time-minder/src/app/app.component.ts
+++ angular/time-minder/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from './shared/input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
