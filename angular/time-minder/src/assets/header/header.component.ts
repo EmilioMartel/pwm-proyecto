7b05dbff6f3945314db8119ae8a133b5568ae80f
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-04-08 13:27:23 +0100

    finish of header component

--- angular/time-minder/src/assets/header/header.component.ts
+++ angular/time-minder/src/assets/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BotonComponent } from "../boton/boton.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
