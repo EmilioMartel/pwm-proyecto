a50e8705865b20757469d1d58574a9e95ddda8ab
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-04-11 09:58:34 +0100

    create and import the footer component

--- angular/time-minder/src/app/app.component.ts
+++ angular/time-minder/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../assets/header/header.component';
+import { FooterComponent } from '../assets/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
