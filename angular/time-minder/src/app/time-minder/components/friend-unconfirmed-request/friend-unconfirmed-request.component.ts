7857eb92626b3e50600c960966db6687093eb54f
EmilioMartel <emilio_martel@outlook.es>
2024-04-15 11:13:30 +0100

    create project folder structure

---
+++ angular/time-minder/src/app/time-minder/components/friend-unconfirmed-request/friend-unconfirmed-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-unconfirmed-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './friend-unconfirmed-request.component.html',
+  styleUrl: './friend-unconfirmed-request.component.css'
+})
+export class FriendUnconfirmedRequestComponent {
+
+}
