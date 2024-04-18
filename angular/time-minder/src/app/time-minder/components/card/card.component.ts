42b4c934cf0b08c42aba86d0d0bd924ffbd20464
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-04-18 09:46:01 +0100

    put the card component in landing page

--- angular/time-minder/src/app/time-minder/components/card/card.component.ts
+++ angular/time-minder/src/app/time-minder/components/card/card.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-  @Input() content: string="";
+  @Input() content: string= "";
   @Input() imagen: string = '';
 
 }
