281dfa01434b6090e3d3c4a264b6d403cafe1dc9
XxMARRExX <xx_marre_xx@hotmail.com>
2024-04-22 11:14:59 +0100

    Importación de los componentes y creación de la estructura de friend-list

--- angular/time-minder/app.routes.ts
+++ angular/time-minder/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DiaryComponent } from './src/app/time-minder/diary/diary.component';
 import { PlanComponent } from './src/app/shared/plan/plan.component';
 import { FormSignInComponent } from './src/app/time-minder/components/form-sign-in/form-sign-in.component'
+import { FriendListComponent } from './src/app/time-minder/friend-list/friend-list.component';
 
 export const routes: Routes = [
 
     {path: 'diary', component: DiaryComponent},
     {path: 'plans', component: PlanComponent},
-    {path: 'sign-in', component: FormSignInComponent}
+    {path: 'sign-in', component: FormSignInComponent},
+    {path: 'friends-list', component: FriendListComponent}
 ];
--- angular/time-minder/src/app/time-minder/friend-list/friend-list.component.ts
+++ angular/time-minder/src/app/time-minder/friend-list/friend-list.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { FriendCardComponent } from '../components/friend-card/friend-card.component';
+import { FriendRequestComponent } from '../components/friend-request/friend-request.component';
+import { FriendUnconfirmedRequestComponent } from '../components/friend-unconfirmed-request/friend-unconfirmed-request.component';
+
 @Component({
   selector: 'app-friend-list',
   standalone: true,
-  imports: [],
+  imports: [FriendCardComponent, FriendRequestComponent, FriendUnconfirmedRequestComponent],
   templateUrl: './friend-list.component.html',
   styleUrl: './friend-list.component.css'
 })
