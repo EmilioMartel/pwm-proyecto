e0ed1d05004d15bae42f0681f467cc0ca2cdf169
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-04-29 19:12:07 +0100

    cursor: pointer;

--- angular/time-minder-pwm/src/app/auth/login/login.component.ts
+++ angular/time-minder-pwm/src/app/auth/login/login.component.ts
@@ -6,11 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  @Input() contenidoCorreo: string="Correo"
+  @Input() contenidoCorreo: string="Correo:"
   @Input() tipo: string="email"
   @Input() patron: string="^[^\s@]+@[^\s@]+.[^\s@]+$"
   @Input() id: string="mail"
-  @Input() contenidoContrasena: string="Contraseña"
+  @Input() contenidoContrasena: string="Contraseña:"
   @Input() tipoContrasena: string="password"
   @Input() minlength: string="6"
   @Input() idContrasena: string="mail"
