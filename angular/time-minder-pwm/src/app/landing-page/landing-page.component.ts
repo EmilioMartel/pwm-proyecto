eef84c19674c6a9c9a808687b212a40fa37feafa
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-04-29 23:43:23 +0100

    fix desing of landing page

--- angular/time-minder-pwm/src/app/landing-page/landing-page.component.ts
+++ angular/time-minder-pwm/src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -11,8 +12,8 @@ export class LandingPageComponent {
   contenido2: string="Crea tu propio espacio personal donde puedes subir y almacenar resúmenes, exámenes y una amplia variedad de recursos. ¡Además, explora los recursos compartidos por tus amigos!"
   contenido3: string="Conéctate fácilmente con tus amigos a través de nuestro chat individual y grupal. Organiza eventos, comparte ideas y mantén la comunicación constante."
   contenido4: string="Personaliza tu perfil a tu manera. Actualiza tu información personal, gestiona tus preferencias y disfruta de una experiencia única adaptada a tus necesidades académicas."
-  imagen1: string="../../assets/imagenes/caracteristica1.png"
-  imagen2: string="../../assets/imagenes/caracteristica2.png"
-  imagen3: string="../../assets/imagenes/caracteristica3.png"
-  imagen4: string="../../assets/imagenes/caracteristica4.png"
+  imagen1: string='../../assets/images/caracteristica1.png'
+  imagen2: string="../../assets/images/caracteristica2.png"
+  imagen3: string="../../assets/images/caracteristica3.png"
+  imagen4: string="../../assets/images/caracteristica4.png"
 }
