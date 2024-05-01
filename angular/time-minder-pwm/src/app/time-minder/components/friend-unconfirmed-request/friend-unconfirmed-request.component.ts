1b3ed1d91ad58f71426f6da6bf3b34fc8c441e68
XxMARRExX <xx_marre_xx@hotmail.com>
2024-05-01 19:35:43 +0100

    Creación de la tarjeta friend-unconfirmed-request

--- angular/time-minder-pwm/src/app/time-minder/components/friend-unconfirmed-request/friend-unconfirmed-request.component.ts
+++ angular/time-minder-pwm/src/app/time-minder/components/friend-unconfirmed-request/friend-unconfirmed-request.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-friend-unconfirmed-request',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './friend-unconfirmed-request.component.css'
 })
 export class FriendUnconfirmedRequestComponent {
-
+  @Input() nombre: string = "Cristian Marrero López";
 }
