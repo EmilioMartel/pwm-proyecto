824c594f8d5e4e7ec8603638b91399346ab15ba0
XxMARRExX <xx_marre_xx@hotmail.com>
2024-05-01 21:01:45 +0100

    Diseño avanzado de la página friend-list

--- angular/time-minder-pwm/src/app/time-minder/friend-list/friend-list.component.ts
+++ angular/time-minder-pwm/src/app/time-minder/friend-list/friend-list.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './friend-list.component.css'
 })
 export class FriendListComponent {
+  componenteActivo: string = 'Amigos';
 
+  mostrarComponente(componente: string) {
+    this.componenteActivo = componente;
+  }
 }
