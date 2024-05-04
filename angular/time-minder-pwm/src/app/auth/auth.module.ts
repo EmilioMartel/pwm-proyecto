0275c18ff8c143a8f3a975d61445a663e297b85c
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2024-05-04 21:12:08 +0100

    eliminate the recover-password component

--- angular/time-minder-pwm/src/app/auth/auth.module.ts
+++ angular/time-minder-pwm/src/app/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth/auth.component';
@@ -11,7 +10,6 @@ import { TimeMinderModule } from '../time-minder/time-minder.module';
 @NgModule({
   declarations: [
     LoginComponent,
-    RecoverPasswordComponent,
     SignInComponent,
     AuthComponent
   ],
